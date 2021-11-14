96a4f0cb2ee81a1af24a87b8c5c0472027ee977a
omwtoswe <92936715+omwtoswe@users.noreply.github.com>
2021-11-13 20:59:22 -0800

    Youtube Video Download Script
    Download Free Videos from Youtube

---
+++ youtube_downloader.py
@@ -0,0 +1,6 @@
+from pytube import YouTube
+
+# link of the video to be downloaded
+yt = YouTube('https://www.youtube.com/watch?v=ZVwwoK7d3Yo')
+yt.streams.filter(progressive=True, file_extension='mp4').order_by(
+    'resolution')[-1].download()
